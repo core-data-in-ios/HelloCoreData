c242ab357eac5851478731df1c0f9a3cbd0fca88
Chris Hand <cj.hand@gmail.com>
2022-09-16 21:16:00 -0700

    Initial Commit

---
+++ HelloCoreData/HelloCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoreDataApp.swift
+//  HelloCoreData
+//
+//  Created by Chris Hand on 9/16/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
